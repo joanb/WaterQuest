2dfa6fa4b8dba42837459f931ff2dc85fdda0920
Daniel Balastegui <dbalastegui@gmail.com>
2015-03-22 00:03:47 +0100

    ranking button to ranking activity

--- app/src/main/java/joandev/waterquest/Views/AcercameActivity.java
+++ app/src/main/java/joandev/waterquest/Views/AcercameActivity.java
@@ -1,17 +1,29 @@
 package joandev.waterquest.Views;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import joandev.waterquest.R;
 
 public class AcercameActivity extends Activity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acercame);
+
+        button=(Button)findViewById(R.id.ranking_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),RanquingActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
