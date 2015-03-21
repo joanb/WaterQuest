42be16d339dce2753a01f7c6cd7acd55a3076217
joandev <joanbarrosogarrido@gmail.com>
2015-03-21 17:37:36 +0100

    Base Commit

---
+++ app/src/main/java/Model/Quiz.java
@@ -0,0 +1,37 @@
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by joanbarroso on 21/3/15.
+ */
+public class Quiz {
+
+
+    private final String QuizQuestion1 = "";
+    private final String Quiz1Response1 = "";
+    private final String Quiz1Response2 = "";
+    private final String Quiz1Response3 = "";
+    private final String Quiz1Response4 = "";
+
+    private final String QuizQuestion2 = "";
+    private final String Quiz2Response1 = "";
+    private final String Quiz2Response2 = "";
+    private final String Quiz2Response3 = "";
+    private final String Quiz2Response4 = "";
+
+    private final String QuizQuestion3 = "";
+    private final String Quiz3Response1 = "";
+    private final String Quiz3Response2 = "";
+    private final String Quiz3Response3 = "";
+    private final String Quiz3Response4 = "";
+
+
+    private ArrayList<ArrayList<String>> quizs;
+
+    public Quiz() {
+//        ArrayList<String> quiz1 = new ArrayLis
+//        quizs = new ArrayList<ArrayList<String>>();
+//        quizs.add
+    }
+}
